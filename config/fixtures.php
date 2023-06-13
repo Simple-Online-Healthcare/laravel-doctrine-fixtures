4950397b65cf802a4e5d15bbde22cee5d9dda51e
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-06-13 14:57:21 +0100

    Bug fix: imploding a string (fixed #1)

--- config/fixtures.php
+++ config/fixtures.php
@@ -7,6 +7,6 @@ return [
      *
      * Must be provided in a comma separated list.
      */
-    'autoloadEnvironments' => implode(',', env('DOCTRINE_FIXTURES_AUTOLOAD_ENVIRONMENTS', 'dev,testing,staging')),
+    'autoloadEnvironments' => explode(',', env('DOCTRINE_FIXTURES_AUTOLOAD_ENVIRONMENTS', 'dev,testing,staging')),
 
 ];
